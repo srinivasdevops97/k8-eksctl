ce7c067e5abe8b305d778b05543056d1a02a852d
srinivasdevops97 <birudurajusrinivas@gmail.com>
2026-07-21 11:10:59 +0530

    kubernetes

--- kubernetes-intall.sh
+++ kubernetes-intall.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-# kubenetes installation
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
 kubectl version
-# eksctl installation
-ARCH=amd64
-PLATFORM=$(uname -s)_$ARCH
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
-ekstcl version
