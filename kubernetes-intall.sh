8f15614d6cb560d79cc74ef9cb9b7f9dfa7e68fd
srinivasdevops97 <birudurajusrinivas@gmail.com>
2026-07-23 17:48:37 +0530

    kubernetes

--- kubernetes-intall.sh
+++ kubernetes-intall.sh
@@ -1,4 +1,4 @@
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.36.2/2026-07-05/bin/linux/amd64/kubectl
-chmod +x /kubectl
+chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
 kubectl version
